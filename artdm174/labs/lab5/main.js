1648825921b4b500cfbbb8717998df6dbb976e30
Josh <lifetrekkerca@gmail.com>
2024-10-24 12:04:50 -0700

    tried to add play button in seperate function

--- artdm174/labs/lab5/main.js
+++ artdm174/labs/lab5/main.js
@@ -6,19 +6,24 @@ function init()
 
 
     const playBtn = document.getElementById("start");
-    playBtn.addEventListener("click", (e) => { player.play() } );
+    playBtn.addEventListener("click", playMedia(player) );
+
+    function playMedia(element)
+    {
+        element.play();
+    }
 
     const pauseBtn = document.getElementById("pause");
-    pauseBtn.addEventListener("click", (e) => { player.pause() } );
+    pauseBtn.addEventListener("click", () => { player.pause() } );
 
     const stopBtn = document.getElementById("stop");
-    stopBtn.addEventListener("click", (e) => { player.pause();  player.currentTime = 0; } );
+    stopBtn.addEventListener("click", () => { player.pause();  player.currentTime = 0; } );
 
     const muteBtn = document.getElementById("mute");
-    muteBtn.addEventListener("click", (e) => { player.volume = 0.0; } );
+    muteBtn.addEventListener("click", () => { player.volume = 0.0; } );
 
     const softerBtn = document.getElementById("softer");
-    softerBtn.addEventListener("click", (e) => { 
+    softerBtn.addEventListener("click", () => { 
         if(player.volume > 0.0)
         {
             player.volume = (player.volume * 10 - 1) / 10;  
@@ -28,7 +33,7 @@ function init()
     } );
 
     const louderBtn = document.getElementById("louder");
-    louderBtn.addEventListener("click", (e) => { 
+    louderBtn.addEventListener("click", () => { 
         if(player.volume < 1.0)
         {
             player.volume = (player.volume * 10 + 1) / 10;  
@@ -36,19 +41,19 @@ function init()
     } );
 
     const ffBtn = document.getElementById("fast-forward");
-    ffBtn.addEventListener("click", (e) => { player.playbackRate = 2 });
+    ffBtn.addEventListener("click", () => { player.playbackRate = 2 });
 
     const fasterBtn = document.getElementById("faster-speed");
-    fasterBtn.addEventListener("click", (e) => { player.playbackRate = 1.5 });
+    fasterBtn.addEventListener("click", () => { player.playbackRate = 1.5 });
 
     const normalBtn = document.getElementById("normal-speed");
-    normalBtn.addEventListener("click", (e) => { player.playbackRate = 1 });
+    normalBtn.addEventListener("click", () => { player.playbackRate = 1 });
 
     const slowerBtn = document.getElementById("slower-speed");
-    slowerBtn.addEventListener("click", (e) => { player.playbackRate = 0.5 });
+    slowerBtn.addEventListener("click", () => { player.playbackRate = 0.5 });
 
     const slowBtn = document.getElementById("slow-motion");
-    slowBtn.addEventListener("click", (e) => { player.playbackRate = 0.25 });
+    slowBtn.addEventListener("click", () => { player.playbackRate = 0.25 });
 
 
     const skipTimes = [0, 84];
@@ -56,15 +61,17 @@ function init()
 
     for(let i = 1; i < skipSpots.children.length; i++)
     {
-        skipSpots.children[i].addEventListener("click", (e) => { player.currentTime = skipTimes[i - 1] })
+        skipSpots.children[i].addEventListener("click", () => { player.currentTime = skipTimes[i - 1] })
     }
 
 
     const hippoBtn = document.getElementById("hippo-option");
-    hippoBtn.addEventListener("click", (e) => { player.src = "hippos.mp4"; player.load(); });
+    hippoBtn.addEventListener("click", () => { player.src = "hippos.mp4"; player.load(); });
 
     const cookingBtn = document.getElementById("cooking-option");
-    cookingBtn.addEventListener("click", (e) => { player.src = "cookingVideo.mp4"; player.load(); });
+    cookingBtn.addEventListener("click", () => { player.src = "cookingVideo.mp4"; player.load(); });
+
+
 
 
 }
